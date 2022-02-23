d50e92cc58e6ac9a0fbdbe01a87dfc970e8e6eb0
fillol <dniel_fillol@hotmail.com>
2022-02-23 18:56:09 -0300

    print no longer needed

--- ABNT/main.go
+++ ABNT/main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"NewABNT/CSV"
 	"NewABNT/Functions"
-	"fmt"
 )
 
-func main()  {
+func main() {
 
 	authors := CSV.ReadCsvFile("NameCSV/Authors.csv")
 
 	abnt := Functions.ABNT(authors)
 
-	CSV.WriteCSV("Autores_ABNT",abnt)
-
-	fmt.Println(abnt[123].TextABNTLong)
+	CSV.WriteCSV("Autores_ABNT", abnt)
 
 }
-
-
